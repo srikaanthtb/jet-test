4c438c501d5e8fb9b571f9d52d7a8d131b8a4582
sri <srikaanth.t.b@gmail.com>
2026-06-26 23:33:46 +0100

    add http endpoints for couriers

---
+++ routes/couriers.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+const { reconciledProfiles } = require('../lib/parse-events');
+
+/* GET all couriers */
+router.get('/', function(req, res, next) {
+  res.json(reconciledProfiles);
+});
+
+/* GET single courier by id (employeeId or globalCourierId) */
+router.get('/:id', function(req, res, next) {
+  const id = req.params.id;
+  const courier = reconciledProfiles.find(p => 
+    p.employeeId === id || p.globalCourierId === id
+  );
+  
+  if (!courier) {
+    return res.status(404).json({ error: 'Courier not found' });
+  }
+  
+  res.json(courier);
+});
+
+module.exports = router;
